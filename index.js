79d17b41ac16387c3395a6b25258872f04285cba
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-24 13:20:26 -0800

    adding routes

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const read = (text) => {
-  return text;
-};
+import figlet from "figlet";
 
-const msg = read("sup wrld");
+const server = Bun.serve({
+  port: 3001,
+  fetch(req) {
+    const url = new URL(req.url);
 
-console.log(msg);
+    if (url.pathname == "/") {
+      const body = figlet.textSync("sup, dawg");
+      return new Response(body);
+    }
+    if (url.pathname === "/about") {
+      return new Response("about me");
+    }
+
+    if (url.pathname === "/contact") {
+      return new Response("conact us");
+    }
+
+    return new Response("404");
+  },
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
